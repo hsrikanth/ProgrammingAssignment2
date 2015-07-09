14d461ba2b0153f6b10633bc92ee98f5725b385d
hsrikanth <sh963r@att.com>
2015-07-08 17:47:47 -0700

    Edit typo

--- cachematrix.R
+++ cachematrix.R
@@ -15,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
 	get <- function() x
 	setinv <- function(solve) inv <<- solve
 	getinv <- function() inv
-	list(set = set,get = get,setinv = setinv,getinv = getinv
+	list(set = set,get = get,setinv = setinv,getinv = getinv)
 }
 
 
